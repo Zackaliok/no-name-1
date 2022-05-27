12bacd5fdf36d98d2d54c6e811844a3d5c9e6e4e
Tristan Fumiere <tristan_fumiere@ens.univ-artois.fr>
2022-05-28 00:42:51 +0200

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Navigation hellow</h1>
+</template>
+
+<script>
+export default {
+  components: {
+    Navigation
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TheWelcome from "@/components/TheWelcome.vue";
+import Navigation from "@/components/Navigation.vue";
+</script>
+
+<template>
+  <main>
+    <!--<TheWelcome />-->
+    <Navigation />
+  </main>
+</template>
